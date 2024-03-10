7ee2ea6d71e3ac36bd421338556136cf016b89bf
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2024-03-10 20:19:14 +0100

    Menu fonctionnelle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ const menuIsOpen = ref(false)
 </script>
 
 <template>
-    <header class="bg-slate-100 shadow-md h-[87px] flex items-center">
+    <header class="bg-slate-100 shadow-md h-[87px] flex items-center relative">
         <nav>
             <div class="flex items-center m-5 space-x-64">
                 <RouterLink to="/"><img src="/src/img/logo_portfolio.svg" alt="Logo Portfolio Gaspard BONDY" class="w-9 h-9 lg:w-auto lg:h-auto"></RouterLink>
@@ -15,14 +15,14 @@ const menuIsOpen = ref(false)
                 </div>
             </div>
         </nav>
-
-        <nav v-show="menuIsOpen" class="bg-red-700 flex">
-            <ul class="">
-                <li><RouterLink to="#">1</RouterLink></li>
-                <li><RouterLink to="#">2</RouterLink></li>
-                <li><RouterLink to="#">3</RouterLink></li>
-                <li><RouterLink to="#">4</RouterLink></li>
-            </ul>
-        </nav>
     </header>
+    <nav v-show="menuIsOpen" class="bg-red-700 absolute z-50 inset-0">
+        <img src="/src/img/croix-menu.webp" alt="croix" class="w-20 mr-0 ml-auto" @pointerdown="menuIsOpen = false">
+        <!-- <img src="/src/img/fleche-menu.webp" alt="croix"> -->
+            <ul class="ml-5 text-white">
+                <li><RouterLink to="#">MY PROJECT</RouterLink></li>
+                <li><RouterLink to="#">ABOUT ME</RouterLink></li>
+                <li><RouterLink to="#">CONTACT</RouterLink></li>
+            </ul>
+    </nav>
 </template>
