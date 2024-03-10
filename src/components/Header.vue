5f1255a84d0370451e41e18520d7649ad9468ed8
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2024-03-10 17:13:50 +0100

    Ajout logo header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-    <header>
+    <header class="bg-black">
+        <div>
+        <img src="/src/img/logo_portfolio.svg" alt="Logo Portfolio Gaspard BONDY" class="m-auto">
         
+        </div>
     </header>
 </template>
