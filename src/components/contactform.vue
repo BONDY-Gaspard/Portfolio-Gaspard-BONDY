1f23390a68a6c842c9eeca0097ef8eb8b5973fbe
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2024-03-21 19:31:41 +0100

    Responsive Contact

--- src/components/contactform.vue
+++ src/components/contactform.vue
@@ -14,26 +14,28 @@ const submitForm = () => {
 };
 </script>
 <template>
-    <form @submit.prevent="submitForm" class="mt-12 mx-16">
+    <form @submit.prevent="submitForm" class="mt-12 mx-16 md:mx-96">
         <div class="space-y-6">
-            <div class="relative z-0">
-                <input type="text" name="name" class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
-                <label label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Name</label>
+            <div class="md:flex md:justify-center md:space-x-12">
+                <div class="relative z-0">
+                    <input type="text" name="name" class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
+                    <label label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Name</label>
+                </div>
+                <div class="relative z-0">
+                    <input type="text" name="email" required class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
+                    <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Email*</label>
+                </div>
+                <div class="relative z-0">
+                    <input type="text" name="subject" class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
+                    <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Subject</label>
+                </div>
             </div>
-            <div class="relative z-0">
-                <input type="text" name="email" required class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
-                <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Email</label>
-            </div>
-            <div class="relative z-0">
-                <input type="text" name="subject" class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" " />
-                <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Subject</label>
-            </div>
-            <div class="relative z-0 col-span-2">
-                <textarea name="message" rows="5" class="peer block w-60 appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+            <div class="relative z-0 col-span-2 md:ml-[168px]">
+                <textarea name="message" rows="5" class="peer block w-60 md:w-[820px] appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-red-700 focus:outline-none focus:ring-0" placeholder=" "></textarea>
                 <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-red-700 peer-focus:dark:text-red-700">Your message here...</label>
             </div>
         </div>
-        <div class="p-12 mx-6">
+        <div class="p-12 mx-6 md:flex md:justify-center md:mt-10">
             <button type="submit" class="w-[112.47px] h-[34.08px] bg-red-700 text-white rounded-full flex items-center -space-x-4 gap-6 font-rubik text-lg font-normal">
                 <p class="ml-5">Send</p>
                 <img src="/src/img/contact/send.svg" class="" alt="Send button">
