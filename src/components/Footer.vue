7a9d5f8d127d5be5f176f06bd638453fc38032e7
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2024-03-23 21:46:02 +0100

    Correction link image

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,34 +24,34 @@
             <div class="flex justify-center mt-12 space-x-5">
                 <div class="flex">
                     <a href="https://www.instagram.com/gaspardtex/" target="_blank">
-                        <img src="/src/img/footer/logo-instagram-footer.svg" alt="Logo instagram" class="absolute opacity-100 hover:opacity-0 size-12">
+                        <img src="/src/img/logo-instagram-footer.svg" alt="Logo instagram" class="absolute opacity-100 hover:opacity-0 size-12">
                     </a>
                     <a href="https://www.instagram.com/gaspardtex/" target="_blank">
-                        <img src="/src/img/footer/logo-instagram-hover-footer.svg" alt="Logo instagram sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
+                        <img src="/src/img/logo-instagram-hover-footer.svg" alt="Logo instagram sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
                     </a>
                 </div>
                 <div class="flex">
                     <a href="https://twitter.com/GaspardTex" target="_blank">
-                        <img src="/src/img/footer/logo-x-footer.svg" alt="Logo X" class="absolute opacity-100 hover:opacity-0 size-12">
+                        <img src="/src/img/logo-x-footer.svg" alt="Logo X" class="absolute opacity-100 hover:opacity-0 size-12">
                     </a>
                     <a href="https://twitter.com/GaspardTex" target="_blank">
-                        <img src="/src/img/footer/logo-x-hover-footer.svg" alt="Logo X sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
+                        <img src="/src/img/logo-x-hover-footer.svg" alt="Logo X sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
                     </a>
                 </div>
                 <div class="flex">
                     <a href="https://www.youtube.com/channel/UCLF_US4rxEGbDht_SYWZgwQ" target="_blank">
-                        <img src="/src/img/footer/logo-youtube-footer.svg" alt="Logo YouTube" class="absolute opacity-100 hover:opacity-0 size-12">
+                        <img src="/src/img/logo-youtube-footer.svg" alt="Logo YouTube" class="absolute opacity-100 hover:opacity-0 size-12">
                     </a>
                     <a href="https://www.youtube.com/channel/UCLF_US4rxEGbDht_SYWZgwQ" target="_blank">
-                        <img src="/src/img/footer/logo-youtube-hover-footer.svg" alt="Logo YouTube sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
+                        <img src="/src/img/logo-youtube-hover-footer.svg" alt="Logo YouTube sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
                     </a>
                 </div>
                 <div class="flex">
                     <a href="https://linktr.ee/gaspardtex" target="_blank">
-                        <img src="/src/img/footer/logo-ensavoir-footer.svg" alt="Logo en savoir plus" class="absolute opacity-100 hover:opacity-0 size-12">
+                        <img src="/src/img/logo-ensavoir-footer.svg" alt="Logo en savoir plus" class="absolute opacity-100 hover:opacity-0 size-12">
                     </a>
                     <a href="https://linktr.ee/gaspardtex" target="_blank">
-                        <img src="/src/img/footer/logo-ensavoir-hover-footer.svg" alt="Logo en savoir plus sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
+                        <img src="/src/img/logo-ensavoir-hover-footer.svg" alt="Logo en savoir plus sélectionné" class="relative opacity-0 hover:opacity-100 size-12">
                     </a>
                 </div>
             </div>
