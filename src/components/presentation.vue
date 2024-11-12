e61f7ffd1f546eaaf664e5d7b247fdab33832ad2
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2024-11-12 13:24:20 +0100

    Modification âge

--- src/components/presentation.vue
+++ src/components/presentation.vue
@@ -6,7 +6,7 @@
     <div class="md:flex md:items-center md:mt-20 md:-space-x-12 md:mx-10">
         <img src="/src/img/photo-gaspard-aboutme.svg" alt="Photo Gaspard" class="m-auto mt-12 md:size-[400px]">
         <div class="my-12 mx-10 space-y-6">
-            <p>Let me introduce myself, I'm Gaspard, I'm 19 and I'm a student at BUT MMI in Montbéliard.</p>
+            <p>Let me introduce myself, I'm Gaspard, I'm 20 and I'm a student at BUT MMI in Montbéliard.</p>
             <p>My main passion is video games, and I love playing different games.</p>
             <p>I also enjoy producing and directing videos on YouTube, as well as doing live lives on Twitch. </p>
             <p>Over the years, I have acquired a wealth of knowledge in various fields such as audiovisual, communication, design and web development...</p>
